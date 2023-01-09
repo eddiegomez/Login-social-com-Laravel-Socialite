b5aad9c80bba0aa2ad6ef5053bc8260f38d0197e
Matondo <matondojoaokitemoco@gmail.com>
2023-01-09 18:58:15 +0100

    Configirando as Rotas de Redirect e Callback

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\GoogleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +19,7 @@ Route::get('/', function () {
 });
 
 Auth::routes(['verify' => true]);
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('auth/google/redirect'.[]);
-
-Route::get('auth/google/callback'.[]);
+Route::get('auth/google/redirect'.[GoogleController::class,'']);
+Route::get('auth/google/redirect'.[GoogleController::class,'']);
