937d3dae1fe6889d4e9099452ad28d5a711707a7
Matondo <matondojoaokitemoco@gmail.com>
2023-01-10 13:44:49 +0100

    Adicionando o Controllador FacebookController

---
+++ app/Http/Controllers/Auth/FacebookController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class FacebookController extends Controller
+{
+    public function handleFacebookRedirect()
+    {
+
+        return Socialite::driver('facebook')->redirect();
+    }
+    public function handleFacebookCallback()
+    {
+        try {
+
+            $user = Socialite::driver('facebook')->user();
+
+            $finduser = User::where('social_id', $user->id)->first();
+
+            if($finduser){
+
+                Auth::login($finduser);
+
+                return redirect()->route('home');
+
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'social_id'=> $user->id,
+                    'social_type'=> 'google',
+                    'password' =>Hash::make('my-facebook')
+                ]);
+
+                Auth::login($newUser);
+
+                return redirect()->route('home');
+            }
+
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
